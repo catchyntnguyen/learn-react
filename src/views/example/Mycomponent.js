dcac48224736d916c22a10f8cacf47daf1b570ac
DuyNTK <149471325+DuyNTK@users.noreply.github.com>
2024-04-23 21:22:36 +0700

    code form react

--- src/views/example/Mycomponent.js
+++ src/views/example/Mycomponent.js
@@ -1,19 +1,30 @@
 import React from 'react';
 class MyComponent extends React.Component{
-    handleClickButton = () => {
-        alert("Click button");
+    handlerChangeFirstName = (event) => {
+        this.setState({firstName: event.target.value});
+    }
+    handlerChangeLastName = (event) => {
+        this.setState({lastName: event.target.value});
+    }
+    handlerSubmit = (event) => {
+        event.preventDefault();
+        console.log(">>> ", this.state);
     }
     state = {
-        name: "catchy"
+        firstName: "",
+        lastName: ""
     }
     render () {
         let name = "Catchy"
         return(
             <>
-                <div className='first'> Nguyễn Trần {this.state.name} </div>
-            <div className='second'>
-                <button onClick={() => {this.handleClickButton()}} >click me</button>
-            </div>
+            <form >
+                <label htmlfor="fname">First name:</label><br/>
+                <input type="text" value={this.state.firstName} onChange={(event) =>{this.handlerChangeFirstName(event)}} /><br/>
+                <label htmlfor="lname">Last name:</label><br/>
+                <input type="text" value={this.state.lastName} onChange={(event) =>{this.handlerChangeLastName(event)}}/><br/><br/>
+                <input type="submit" onClick={(event) => this.handlerSubmit(event)} />
+                </form>
             </>
         )
     }
